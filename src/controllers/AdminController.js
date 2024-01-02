e028c37230112212be557a0880de55a955e75808
alperengokbak <swalperen3008@gmail.com>
2024-01-02 03:52:38 +0300

    Added admin function

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -3,7 +3,20 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function addHotel(req, res) {
-  const { name, address, city, country, price, capacity, image, memberPrice, specialPrice } = req.body;
+  const {
+    name,
+    address,
+    city,
+    country,
+    price,
+    capacity,
+    image,
+    rating,
+    memberPrice,
+    specialPrice,
+    description,
+    features,
+  } = req.body;
   try {
     let newPrice = price;
     if (specialPrice) {
@@ -18,8 +31,11 @@ async function addHotel(req, res) {
         price: newPrice,
         capacity,
         image,
+        rating,
         memberPrice,
         specialPrice,
+        description,
+        features,
       },
     });
     res.json(newHotel);
@@ -29,4 +45,27 @@ async function addHotel(req, res) {
   }
 }
 
-export { addHotel };
+async function deleteHotel(req, res) {
+  const { id } = req.body;
+  try {
+    const hotel = await prisma.hotel.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!hotel) {
+      return res.status(400).json({ error: "Hotel does not exist" });
+    }
+    await prisma.hotel.delete({
+      where: {
+        id,
+      },
+    });
+    res.json({ status: "success" });
+  } catch (error) {
+    console.log(error);
+    res.json({ error: "Unable to delete hotel" });
+  }
+}
+
+export { addHotel, deleteHotel };
--- src/routes/AdminRoute.js
+++ src/routes/AdminRoute.js
@@ -1,10 +1,11 @@
 import Router from "express";
 import { isAdmin } from "../middleware.js";
-import { addHotel } from "../controllers/AdminController.js";
+import { addHotel, deleteHotel } from "../controllers/AdminController.js";
 
 const router = Router();
 router.use(isAdmin);
 
 router.post("/add-hotel", addHotel);
+router.delete("/delete-hotel", deleteHotel);
 
 export default router;
